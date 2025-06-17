4be4c1f9dc7885f92ed503e308ffaf552d704b86
davidantoni0 <davidantoniopetro@gmail.com>
2025-06-17 16:37:32 -0300

    Add: new files to page.

---
+++ node-project/src/components/bandeira_polonia.js
@@ -0,0 +1,15 @@
+import styles from './bandeira_polonia.module.css'
+
+export default function BandeiraPolonia(){
+
+    return(
+
+    
+        <div className={styles.bandeira}>
+            <div className={styles.faixaBranca}>
+            </div>
+            <div className={styles.faixaVermelha}>
+            </div>
+        </div>
+    )
+}
